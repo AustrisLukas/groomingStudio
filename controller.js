de945df8b4b088792a6ecaba14bf179ff242f586
AustrisLukas <austris.lukas@gmail.com>
2025-07-03 21:56:11 +0100

    booking system v0.1

--- controller.js
+++ controller.js
@@ -5,5 +5,11 @@ exports.renderHome = async (req,res) =>{
     console.log('App: Processing renderHome');
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
     
+}
+
+exports.renderAppointments = async (req,res) => {
+
+    console.log('App: Processing renderAppointments');
+    res.sendFile(path.join(__dirname, 'public', 'appointments.html'));
 
 }
--- routes.js
+++ routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller.js');
 
+router.get('/appointments', controller.renderAppointments);
 router.get('/', controller.renderHome);
+
 
 
 module.exports = router;
